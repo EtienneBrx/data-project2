ce070d68b9183b651971a7dc9f6a52f101d48836
Etienne <etienne.berrie@u-psud.fr>
2021-01-14 17:56:36 +0100

    Ouverture Fichier

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from pykml import parser
+from pykml.parser import Schema
+from lxml import etree
+import xml.etree.ElementTree as ET
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    FICHIER_1 = "Polytech_mymap.xml"
+    FICHIER_2 = "Polytech_earth.kml"
 
-    
+    with open(FICHIER_1) as file:
+        doc = parser.parse(file)
+        print('doc opened')
 
+        schema_ogc = Schema("ogckml22.xsd")
+        schema_gx = Schema("kml22gx.xsd")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        if schema_ogc.validate(doc):
+            print("Fichier ogc")
+        elif schema_gx.validate(doc):
+            print("Fichier gx")
+        else:
+            print("Erreur format fichier")
+            schema_gx.assertValid(doc)
+
+        print(etree.tostring(doc))
+
+        root = ET.parse(FICHIER_1).getroot()
+        for root.iter("Placemark"):
+
