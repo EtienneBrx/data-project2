28e1702eb77ed6aee2e211e85098a01b552de7e4
Etienne <etienne.berrie@u-psud.fr>
2021-01-15 10:19:38 +0100

    Lecture fichier

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from pykml import parser
 from pykml.parser import Schema
 from lxml import etree
-import xml.etree.ElementTree as ET
 
 
 if __name__ == '__main__':
     FICHIER_1 = "Polytech_mymap.xml"
     FICHIER_2 = "Polytech_earth.kml"
 
+    demandes_map = {"parking" : 0,
+                    "parc": 0,
+                    "potager": 0,
+                    "piscine":0,
+                    "ressourcerie": 0,
+                    "gymnase":0,
+                    "terrain de sport":0,
+                    "place":0
+                    }
+    polygones = []
+
     with open(FICHIER_1) as file:
-        doc = parser.parse(file)
+        doc = parser.parse(file).getroot()
         print('doc opened')
 
         schema_ogc = Schema("ogckml22.xsd")
@@ -23,8 +33,17 @@ if __name__ == '__main__':
             print("Erreur format fichier")
             schema_gx.assertValid(doc)
 
-        print(etree.tostring(doc))
+        #print(etree.tostring(doc))
+        print(doc.Document.name)
 
-        root = ET.parse(FICHIER_1).getroot()
-        for root.iter("Placemark"):
+        for pm in doc.Document.Placemark:
+            # print(pm.name)
+            if str(pm.name).lower() in demandes_map.keys():
+                demandes_map[str(pm.name).lower()] += 1
+                # print(pm.Polygon.outerBoundaryIs.LinearRing.coordinates)
+                polygones.append(pm)
+
+        print(demandes_map)
+        print(len(polygones))
+
 
